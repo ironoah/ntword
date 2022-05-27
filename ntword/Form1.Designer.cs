d5b15c14081d3fb27c7ad7eec9a21477ce2d5d2c
tanigawa <tanigawa@axislink.co.jp>
2022-05-27 15:33:45 +0900

    first commit

---
+++ ntword/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace ntword
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNext = new System.Windows.Forms.Button();
+            this.chk1 = new System.Windows.Forms.CheckBox();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnNext
+            // 
+            this.btnNext.Font = new System.Drawing.Font("MS UI Gothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnNext.Location = new System.Drawing.Point(261, 146);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(369, 86);
+            this.btnNext.TabIndex = 0;
+            this.btnNext.Text = "NEXT";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chk1
+            // 
+            this.chk1.AutoSize = true;
+            this.chk1.Font = new System.Drawing.Font("MS UI Gothic", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.chk1.Location = new System.Drawing.Point(32, 12);
+            this.chk1.Name = "chk1";
+            this.chk1.Size = new System.Drawing.Size(193, 101);
+            this.chk1.TabIndex = 2;
+            this.chk1.Text = "xxx";
+            this.chk1.UseVisualStyleBackColor = true;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Font = new System.Drawing.Font("MS UI Gothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnBack.Location = new System.Drawing.Point(32, 146);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(223, 86);
+            this.btnBack.TabIndex = 3;
+            this.btnBack.Text = "BACK";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 262);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.chk1);
+            this.Controls.Add(this.btnNext);
+            this.Name = "Form1";
+            this.Text = "単語帳";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.CheckBox chk1;
+        private System.Windows.Forms.Button btnBack;
+    }
+}
+
---
+++ ntword/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ntword
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, string> m_dic = new Dictionary<string, string>();
+        bool m_ejToggle = false;
+        string m_thiskey = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            m_dic["CA"] = "California";
+            m_dic["TX"] = "Texas";
+            m_dic["instruction"] = "指示";
+
+            m_thiskey = this.GetRandomKey();
+            chk1.Text = m_thiskey;
+            m_ejToggle = false;
+            btnBack.Enabled = false;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (m_ejToggle)
+            {
+                // E
+                m_thiskey = this.GetRandomKey();
+                chk1.Text = m_thiskey;
+                btnBack.Enabled = false;
+            }
+            else
+            {
+                // J
+                chk1.Text = m_dic[m_thiskey];
+                btnBack.Enabled = true;
+            }
+            m_ejToggle = m_ejToggle ? false : true; // 反転
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            chk1.Text = m_thiskey;
+            btnBack.Enabled = false;
+        }
+
+        private string GetRandomKey()
+        {
+            Random rnd = new Random();
+            // Generate random indexes for pet names.
+            int mIndex = rnd.Next(m_dic.Count);
+
+            int t = 0;
+            foreach (string s in m_dic.Keys)
+            {
+                if (t == mIndex) return s;
+                t++;
+            }
+
+            return "Nothing";
+        }
+
+    }
+}
